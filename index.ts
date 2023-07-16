83b95f63114451d386e75d5e6620e0fd0167e1f3
Mustafa Ekşi <mustafa.eskieksi@gmail.com>
2023-07-16 16:56:00 +0300

    mongodb eklendi

--- index.ts
+++ index.ts
@@ -1,12 +1,36 @@
+import { ObjectId } from "@fastify/mongodb";
+
 const fastify = require('fastify')()
 
-fastify.register(require('fastify-sqlite'), {dbFile: 'db.db'})
+const ALLOWED_ORIGINS = ['http://localhost:8080/']
 
-fastify.get("/getproducts", function (req: any, res: any){
-  fastify.sqlite.all("select * from products", (err: any, rows: any)=>{
-    res.send(err || rows);
-  })
-})
+fastify.register(require('@fastify/mongodb'), {
+  forceClose: true,
+  url: 'mongodb://127.0.0.1:27017/eticaret'
+});
+fastify.get("/getproducts", async function (req:any, res:any) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Origin, X-Requested-With, Content-Name, Accept, Sdk-Context');
+
+  var products = fastify.mongo.db.collection('products');
+  let p = await products.find().toArray();
+  console.log(p)
+  res.send(p)
+});
+
+fastify.post("/getproduct", async function (req:any, res:any) {
+  let orig = ALLOWED_ORIGINS[0];
+  res.header('Access-Control-Allow-Origin', orig);
+  res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+  res.header('Access-Control-Max-Age', '0');
+  var products = fastify.mongo.db.collection('products');
+  console.log(JSON.parse(req.body))
+  let p = await products.findOne({_id:new ObjectId(req.body.productid)});
+  console.log(p)
+  res.send(p)
+});
 
 fastify.listen({ port: 3000 }, (err:any) => {
   if (err) throw err
