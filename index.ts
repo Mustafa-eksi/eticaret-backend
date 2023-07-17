4c1305b99fed8f69f7b700b84afd21cd1be7ee99
Mustafa-eksi <mustafa.eskieksi@gmail.com>
2023-07-17 21:23:56 +0300

    CORS hatasının çözümü: Hata sanırsam fastify'ın varsayılan olarak OPTIONS requestine cevap vermemesi yüzünden oluyormuş. @fastify/cors uzantısı OPTIONS requestine cevap veriyor böylece sorun çözülüyor.

--- index.ts
+++ index.ts
@@ -2,17 +2,17 @@ import { ObjectId } from "@fastify/mongodb";
 
 const fastify = require('fastify')()
 
-const ALLOWED_ORIGINS = ['http://localhost:8080/']
-
 fastify.register(require('@fastify/mongodb'), {
   forceClose: true,
   url: 'mongodb://127.0.0.1:27017/eticaret'
 });
-fastify.get("/getproducts", async function (req:any, res:any) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Origin, X-Requested-With, Content-Name, Accept, Sdk-Context');
+fastify.register(require('@fastify/cors'), {
+  origin: ['http://localhost:8080'],
+  methods: ["GET", "POST", "OPTIONS"],
+  maxAge: 0
+})
 
+fastify.get("/getproducts", async function (req:any, res:any) {
   var products = fastify.mongo.db.collection('products');
   let p = await products.find().toArray();
   console.log(p)
@@ -20,13 +20,8 @@ fastify.get("/getproducts", async function (req:any, res:any) {
 });
 
 fastify.post("/getproduct", async function (req:any, res:any) {
-  let orig = ALLOWED_ORIGINS[0];
-  res.header('Access-Control-Allow-Origin', orig);
-  res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
-  res.header('Access-Control-Max-Age', '0');
   var products = fastify.mongo.db.collection('products');
-  console.log(JSON.parse(req.body))
+  console.log(req.body)
   let p = await products.findOne({_id:new ObjectId(req.body.productid)});
   console.log(p)
   res.send(p)
